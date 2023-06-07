58e4f7cbec90ec1f0f827d0342d9dbd7c80f05bd
Ugwu Adaeze <adaezeugwu6@gmail.com>
2023-06-07 15:59:04 +0100

    worked on the why choose scissors section

--- src/components/adaeze.tsx
+++ src/components/adaeze.tsx
@@ -1,8 +1,62 @@
 const Adaeze = () => {
   return (
-    <div className="text-[16px] text-green-800 px-8 md:text-[28px]">
-      <p>Hi, Adaeze</p>
+    <div
+      id="vission&mission"
+      className="max-w-7xl w-[100%] lg:w-[80%] mx-[auto] py-[50px]">
+          <div className=" mt-[20px] p-4 lg:flex justify-between">
+           
+            <div className="">
+            
+                
+                <h3 className="font-bold text-[24px] lg:text-[2rem] mb-[15px] border-l-4 border-blue-400">
+                    {" "}
+                    Why Choose <span className="text-[#053AAC]"> Scissors </span>
+                </h3>
+              
+                
+                <p className=" max-w-7xl w-[85%] text-[12px] lg:text-[15px]">Scissors is the hub of everything that has to do with your link management. We shorten your URLs, allow you creating custom ones for your personal, business, event usage. Our swift QR code creation, management and usage tracking with advance analytics for all of these is second to none. </p>
+            </div>
+           
+        <div className="  max-w-7xl w-[85%] md:w-[75%] lg::w-[50%]  ">
+            <div className="md:flex justify-center">
+            <div className=" w-[250px]  p-[20px]" >
+                <img className="h-[1.5rem] w-[1.5rem] rounded-[50%] bg-[#3284FF] p-[5px]" src="https://res.cloudinary.com/dzfbm5ddp/image/upload/v1686138208/link-2_s0ja0u.png" alt="url picture" />
+                <h2 className="text-[#F6A32D] mt-[5px]">URL Shortening</h2>
+             
+                <p className="  text-[12px] mt-[5px] ">Scissor allows you to shorten URLs of your business, events. Shorten your URL at scale, URL redirects.</p>
+            </div>
+           
+            <div className=" w-[250px]  m-[10px] p-[20px]" >
+                <img className="h-[1.5rem] w-[1.5rem] rounded-[50%] bg-[#3284FF] p-[5px]" src="https://res.cloudinary.com/dzfbm5ddp/image/upload/v1686138208/edit_swumpd.png" alt="custom picture" />
+                <h2 className="text-[#F6A32D] mt-[5px]">Custom URLs</h2>
+               
+                <p className="text-[12px] mt-[5px] ">With Scissor, you can create custom URLs, with the length you want! A solution for socials and businesses.</p>
+            </div>
+
+            </div>
+            <div className="md:flex justify-center ">
+           
+            <div className=" w-[250px] m-[10px] p-[20px]">
+                <img className="h-[1.5rem] w-[1.5rem] rounded-[50%] bg-[#3284FF] p-[5px] " src="https://res.cloudinary.com/dzfbm5ddp/image/upload/v1686138208/grid_omuhrp.png" alt="qr picture" />
+                <h2 className="text-[#F6A32D] mt-[5px]">QR Codes</h2>
+          
+                <p className="text-[12px] mt-[5px]">Generate QR codes to your business, events. Bring your audience and customers to your doorstep with this scan and go solution.</p>
+            </div>
+            <div className=" w-[250px] m-[10px] p-[20px]">
+                <img className="h-[1.5rem] w-[1.5rem] rounded-[50%] bg-[#3284FF] p-[5px]" src="https://res.cloudinary.com/dzfbm5ddp/image/upload/v1686138208/Vector_1_zk9xdk.png" alt="data pictures" />
+                <h2 className="text-[#F6A32D] mt-[5px]">Data Analytics</h2>
+                
+                <p className="text-[12px] mt-[5px]">
+                Receive data on the usage of either your shortened URL, custom URLs or generated QR codes. Embedded to monitor progress.
+                </p>
+            </div>      
+            </div>
+            
+        </div>
     </div>
+          </div>
+
+                
   );
 };
 
