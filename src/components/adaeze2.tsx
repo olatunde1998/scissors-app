978886ad120bac77ac1718ddd887af0e566c50c5
Ugwu Adaeze <adaezeugwu6@gmail.com>
2023-06-07 15:59:38 +0100

    worked on the pricing  section

---
+++ src/components/adaeze2.tsx
@@ -0,0 +1,102 @@
+
+
+const Adaeze2 = () => {
+  return (
+    <div
+       
+        className="max-w-7xl w-[100%] lg:w-[75%] mx-[auto] py-[50px]">
+        <div className="max-w-7xl w-[100%] lg:w-[80%] mx-[auto] px-[20px] mb-[50px]">
+            <h3 className=" font-bold text-[18px] text-center border-l-4 border-blue-400  lg:text-[2rem]">
+             A<span className="text-[#005AE2]"> price perfect </span> for your needs.{" "}
+            </h3>
+            <p className="w-[100%] lg: text-center">From catering for your personal, business, event, socials needs, you can be rest assured we have you in mind in our pricing.</p>
+        </div>
+        <div className="  max-w-7xl w-[85%] md:w-[75%] lg::w-[50%] mx-[auto]  ">
+            <div className="md:flex justify-center">
+               
+            <div className=" w-[250px]  m-[10px] p-[30px] border-3 border-800-blue hover:-translate-y-3  hover:scale-110 duration-300 transition ease-out-in hover:bg-[#1E3448] hover:text-white " >
+                    <h4 className="mb-[10px]">Basics</h4>
+                    <h2 className="font-bold mt-[5px]">Free</h2>
+                
+                    <p className="text-[12px] mt-[5px] ">Free plan for all users</p>
+                    <div className="flex  items-center text-[12px] mt-[1rem]">
+                        <img className="h-[1rem] mr-[5px] " src="https://res.cloudinary.com/dzfbm5ddp/image/upload/v1686138208/check-circle_hnk58z.png" />
+                        <p>Unlimited URL Shortening</p>
+                    </div>
+                    <div className="flex items-center text-[12px] mt-[1rem] ">
+                        <img className="h-[1rem] mr-[5px]" src="https://res.cloudinary.com/dzfbm5ddp/image/upload/v1686138208/check-circle_hnk58z.png" />
+                        <p>Basic Link Analytics</p>
+                    </div>
+                    <div className="flex text-[12px] items-center mt-[1rem] ">
+                        <img className="h-[1rem] mr-[5px]" src="https://res.cloudinary.com/dzfbm5ddp/image/upload/v1686138208/check-circle_hnk58z.png" />
+                        <p>Customizable Short Links</p>
+                    </div>
+                    <div className="flex text-[12px] items-center mt-[1rem] ">
+                        <img className="h-[1rem] mr-[5px]" src="https://res.cloudinary.com/dzfbm5ddp/image/upload/v1686138208/check-circle_hnk58z.png" />
+                        <p>Standard Support</p>
+                    </div>
+                    <div className="flex text-[12px] items-center mt-[1rem] ">
+                        <img className="h-[1rem] mr-[5px]" src="https://res.cloudinary.com/dzfbm5ddp/image/upload/v1686138208/check-circle_hnk58z.png" />
+                        <p>Ad-supported</p>
+                    </div>
+                </div>
+                <div className=" w-[250px]  m-[10px] p-[30px] hover:-translate-y-3  hover:scale-110 duration-300 transition ease-out-in hover:bg-[#1E3448] hover:text-white   " >
+                    <h4  className="mb-[10px]">Professional</h4>
+                    <h2 className="font-bold mt-[5px]">$15/month</h2>
+                
+                    <p className="text-[12px] mt-[5px] ">Ideal for business creators</p>
+                    <div className="flex  items-center text-[12px] mt-[1rem]">
+                        <img className="h-[1rem] mr-[5px]" src="https://res.cloudinary.com/dzfbm5ddp/image/upload/v1686138208/check-circle_hnk58z.png" />
+                        <p>Enhanced Link Analytics</p>
+                    </div>
+                    <div className="flex items-center text-[12px] mt-[1rem] ">
+                        <img className="h-[1rem] mr-[5px]" src="https://res.cloudinary.com/dzfbm5ddp/image/upload/v1686138208/check-circle_hnk58z.png" />
+                        <p>Custom Branded Domains</p>
+                    </div>
+                    <div className="flex text-[12px] items-center mt-[1rem] ">
+                        <img className="h-[1rem] mr-[5px]" src="https://res.cloudinary.com/dzfbm5ddp/image/upload/v1686138208/check-circle_hnk58z.png" />
+                        <p>Advanced Link Customization</p>
+                    </div>
+                    <div className="flex text-[12px] items-center mt-[1rem] ">
+                        <img className="h-[1rem] mr-[5px]" src="https://res.cloudinary.com/dzfbm5ddp/image/upload/v1686138208/check-circle_hnk58z.png" />
+                        <p>Priority Support</p>
+                    </div>
+                    <div className="flex text-[12px] items-center mt-[1rem] ">
+                        <img className="h-[1rem] mr-[5px]" src="https://res.cloudinary.com/dzfbm5ddp/image/upload/v1686138208/check-circle_hnk58z.png" />
+                        <p>Ad-free Experience</p>
+                    </div>
+                </div>
+                <div className="  w-[250px]  m-[10px] p-[30px] rounded-lg hover:-translate-y-3  hover:scale-110 duration-300 transition ease-in-out hover:bg-[#1E3448] hover:text-white " >
+                    <h4  className="mb-[10px]">Teams</h4>
+                    <h2 className="font-bold mt-[5px]">$25/month</h2>
+                
+                    <p className="text-[12px] mt-[5px] ">Share with up to 10 users</p>
+                    <div className="flex  items-center text-[12px] mt-[1rem]">
+                        <img className="h-[1rem] mr-[5px]" src="https://res.cloudinary.com/dzfbm5ddp/image/upload/v1686138208/check-circle_hnk58z.png" />
+                        <p>Team Collaboration</p>
+                    </div>
+                    <div className="flex items-center text-[12px] mt-[1rem] ">
+                        <img className="h-[1rem] mr-[5px]" src="https://res.cloudinary.com/dzfbm5ddp/image/upload/v1686138208/check-circle_hnk58z.png" />
+                        <p>User Roles and Permissions</p>
+                    </div>
+                    <div className="flex text-[12px] items-center mt-[1rem] ">
+                        <img className="h-[1rem] mr-[5px]" src="https://res.cloudinary.com/dzfbm5ddp/image/upload/v1686138208/check-circle_hnk58z.png" />
+                        <p>Enhanced Security</p>
+                    </div>
+                    <div className="flex text-[12px] items-center mt-[1rem] ">
+                        <img className="h-[1rem] mr-[5px]" src="https://res.cloudinary.com/dzfbm5ddp/image/upload/v1686138208/check-circle_hnk58z.png" />
+                        <p>API Access</p>
+                    </div>
+                    <div className="flex text-[12px] items-center mt-[1rem] ">
+                        <img className="h-[1rem] mr-[5px]" src="https://res.cloudinary.com/dzfbm5ddp/image/upload/v1686138208/check-circle_hnk58z.png" />
+                        <p>Dedicated Account Manager</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+            
+    </div>
+  )
+}
+
+export default Adaeze2;
